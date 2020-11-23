b4f388d352dffb310f1a53947ba693b46b0ce51a
Justin Chadwell <me@jedevc.com>
2020-11-23 10:05:07 +0000

    Better checking of block references

--- synth/passes/typer.py
+++ synth/passes/typer.py
@@ -1,7 +1,10 @@
 from ..parser import (
     AssignmentNode,
+    BlockNode,
+    CallNode,
     DeclarationNode,
     ProcessingError,
+    SpecNode,
     TraversalVisitor,
     VariableNode,
 )
@@ -11,6 +14,32 @@ class TypeCheckVisitor(TraversalVisitor):
     def __init__(self):
         super().__init__()
         self.vars = {}
+
+        self.blocks = {}
+        self.block_refs = {}
+
+    def visit_spec(self, node: SpecNode):
+        super().visit_spec(node)
+
+        # resolve block references after traversal
+        for block_name in self.block_refs:
+            if block_name not in self.blocks:
+                raise ProcessingError(
+                    self.block_refs[block_name], f"block {block_name} is not defined"
+                )
+
+    def visit_block(self, node: BlockNode):
+        if node.name in self.blocks:
+            raise ProcessingError(node, f"block {node.name} cannot be defined twice")
+
+        self.blocks[node.name] = node
+
+        super().visit_block(node)
+
+    def visit_call(self, node: CallNode):
+        self.block_refs[node.target] = node
+
+        super().visit_call(node)
 
     def visit_declaration(self, node: DeclarationNode):
         if node.name in self.vars:
