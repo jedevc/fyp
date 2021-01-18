e746b2d76515df2275cbed7753085f84a858157f
Justin Chadwell <me@jedevc.com>
2021-01-18 10:25:56 +0000

    Add inline interpretation for blocks

--- synth/interpret.py
+++ synth/interpret.py
@@ -1,3 +1,4 @@
+import random
 from typing import Iterable, List
 
 from .graph import (
@@ -15,14 +16,22 @@ from .graph import (
 
 class Interpreter:
     def __init__(self, blocks: List[Block], chunks: ChunkSet):
-        self.blocks = blocks
+        self.blocks = {block.name: block for block in blocks}
         self.chunks = chunks
+
+        self.func_blocks = {block.name for block in blocks if random.random() > 0.5}
+        self.inline_blocks = {
+            block.name for block in blocks if block.name not in self.func_blocks
+        }
 
     def program(self) -> Program:
         final = Program()
 
-        for block in self.blocks:
-            func = FunctionDefinition(block.name, [])
+        for blname, block in self.blocks.items():
+            if blname != "main" and blname not in self.func_blocks:
+                continue
+
+            func = FunctionDefinition(blname, [])
             for stmt in self._transform(block.statements):
                 func.add_statement(stmt)
             final.add_function(func)
@@ -37,8 +46,14 @@ class Interpreter:
     def _transform(self, stmts: Iterable[Statement]) -> Iterable[Statement]:
         for stmt in stmts:
             if isinstance(stmt, Call):
-                new_stmt = ExpressionStatement(Function(stmt.block.name, []))
-                yield new_stmt
+                if stmt.block.name in self.func_blocks:
+                    new_stmt = ExpressionStatement(Function(stmt.block.name, []))
+                    yield new_stmt
+                elif stmt.block.name in self.inline_blocks:
+                    for st in self._transform(self.blocks[stmt.block.name].statements):
+                        yield st
+                else:
+                    raise RuntimeError()
             elif isinstance(stmt, If):
                 yield If(
                     stmt.condition,
