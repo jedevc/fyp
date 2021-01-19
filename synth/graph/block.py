31d09cf02db815f2e694dbc7b533a7e6e7ff3abe
Justin Chadwell <me@jedevc.com>
2021-01-19 10:36:25 +0000

    Traverse the program to find blocks that reference a chunk
    We introduce a function to find the path to all blocks that reference a
    given chunk. This is vital for constructing the algorithm to allocate
    variables onto the stack.

--- synth/graph/block.py
+++ synth/graph/block.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, Tuple, Union
+from typing import Any, Callable, List, Optional, Tuple, Union
 
 from ..node import Operator as OperatorType
 from .chunk import Chunk, ChunkVariable, merge_chunks
@@ -12,11 +12,20 @@ class ExpressionStatement:
     def __init__(self, expr: Expression):
         self.expr = expr
 
+    def traverse(self, func: Callable[[Any], None]):
+        func(self)
+        self.expr.traverse(func)
+
 
 class Block:
     def __init__(self, name: str):
         self.name = name
         self.statements: List[Statement] = []
+
+    def traverse(self, func: Callable[[Any], None]):
+        func(self)
+        for stmt in self.statements:
+            stmt.traverse(func)
 
     def add_statement(self, statement: Statement):
         self.statements.append(statement)
@@ -48,15 +57,28 @@ class Assignment:
         self.target = target
         self.value = value
 
+    def traverse(self, func: Callable[[Any], None]):
+        func(self)
+        self.target.traverse(func)
+        self.value.traverse(func)
+
 
 class Deref:
     def __init__(self, target: Lvalue):
         self.target = target
 
+    def traverse(self, func: Callable[[Any], None]):
+        func(self)
+        self.target.traverse(func)
+
 
 class Ref:
     def __init__(self, target: Lvalue):
         self.target = target
+
+    def traverse(self, func: Callable[[Any], None]):
+        func(self)
+        self.target.traverse(func)
 
 
 class Array:
@@ -64,10 +86,24 @@ class Array:
         self.target = target
         self.index = index
 
+    def traverse(self, func: Callable[[Any], None]):
+        func(self)
+        self.target.traverse(func)
+        self.index.traverse(func)
+
 
 class Call:
     def __init__(self, block: Block):
         self.block = block
+
+    def traverse(self, func: Callable[[Any], None]):
+        func(self)
+
+        # NOTE: do *not* traverse here, as infinite loops can occur, instead it
+        # should be up to the caller to handle appropriately
+
+    def __repr__(self) -> str:
+        return f"<Call to {self.block.name}>"
 
 
 class If:
@@ -77,17 +113,36 @@ class If:
     ):
         self.groups = groups
 
+    def traverse(self, func: Callable[[Any], None]):
+        func(self)
+        for expr, stmts in self.groups:
+            if expr is not None:
+                expr.traverse(func)
+            for stmt in stmts:
+                stmt.traverse(func)
+
 
 class While:
     def __init__(self, condition: Expression, stmts: List[Statement]):
         self.condition = condition
         self.statements = stmts
 
+    def traverse(self, func: Callable[[Any], None]):
+        func(self)
+        self.condition.traverse(func)
+        for stmt in self.statements:
+            stmt.traverse(func)
+
 
 class Function:
     def __init__(self, func: str, args: List[Expression]):
         self.func = func
         self.args = args
+
+    def traverse(self, func: Callable[[Any], None]):
+        func(self)
+        for arg in self.args:
+            arg.traverse(func)
 
 
 class Operation:
@@ -96,13 +151,24 @@ class Operation:
         self.left = left
         self.right = right
 
+    def traverse(self, func: Callable[[Any], None]):
+        func(self)
+        self.left.traverse(func)
+        self.right.traverse(func)
+
 
 class Variable:
     def __init__(self, variable: str, chunk: Optional[Chunk]):
         self.variable = variable
         self.chunk = chunk
 
+    def traverse(self, func: Callable[[Any], None]):
+        func(self)
+
 
 class Value:
     def __init__(self, value: str):
         self.value = value
+
+    def traverse(self, func: Callable[[Any], None]):
+        func(self)
--- synth/interpret.py
+++ synth/interpret.py
@@ -11,6 +11,8 @@ from .graph import (
     If,
     Program,
     Statement,
+    Variable,
+    While,
 )
 
 
@@ -20,13 +22,16 @@ class Interpreter:
 
         self.func_blocks = {block for block in blocks if random.random() > 0.5}
         self.inline_blocks = {
-            block for block in blocks if block.name not in self.func_blocks
+            block for block in blocks if block not in self.func_blocks
         }
 
         self.chunks = chunks
         self.extern = extern
 
-        self.global_chunks = chunks
+        self.global_chunks = {chunk for chunk in chunks if random.random() > 0.5}
+        self.stack_chunks = {
+            chunk for chunk in chunks if chunk not in self.global_chunks
+        }
 
     def program(self) -> Program:
         final = Program()
@@ -38,6 +43,7 @@ class Interpreter:
             func = FunctionDefinition(blname, [])
             for stmt in self._transform(block.statements):
                 func.add_statement(stmt)
+            # TODO: can add function arguments here
             final.add_function(func)
 
         for chunk in self.chunks:
@@ -48,6 +54,28 @@ class Interpreter:
             final.add_extern(var)
 
         return final
+
+    def _find_chunk_refs(
+        self, chunk: Chunk, base: Block, prefix: List[Call]
+    ) -> List[List[Call]]:
+        matches = False
+        paths = []
+
+        def finder(part):
+            nonlocal matches, paths
+
+            if isinstance(part, Variable):
+                if part.chunk == chunk:
+                    matches = True
+            elif isinstance(part, Call):
+                newpaths = self._find_chunk_refs(chunk, part.block, prefix + [part])
+                paths.extend(newpaths)
+
+        base.traverse(finder)
+
+        if matches:
+            paths.append(prefix)
+        return paths
 
     def _transform(self, stmts: Iterable[Statement]) -> Iterable[Statement]:
         for stmt in stmts:
@@ -67,5 +95,7 @@ class Interpreter:
                         for condition, stmts in stmt.groups
                     ]
                 )
+            elif isinstance(stmt, While):
+                yield While(stmt.condition, list(self._transform(stmt.statements)))
             else:
                 yield stmt
