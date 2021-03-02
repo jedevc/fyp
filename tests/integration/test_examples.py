73e2afa1dae554b31a9c8110ea5ee7383f857f1f
Justin Chadwell <me@jedevc.com>
2021-03-02 12:20:54 +0000

    Make tests more robust and test the debug printing

--- tests/integration/test_examples.py
+++ tests/integration/test_examples.py
@@ -29,20 +29,25 @@ def test_synth(example, functions, tmp_path):
     with open(path) as f:
         source_code = f.read()
 
-    output = io.StringIO()
-    synth.synthesize(source_code, output)
-    program_code = output.getvalue()
-    output.close()
+    for _ in range(5):
+        # Because the synthesis process is partly random, we repeat the
+        # generation multiple times to try and ensure we acheive adequate
+        # coverage.
 
-    gen = FunctionGenerator()
-    for args in functions:
-        func, args = args[0], args[1:]
-        func(gen, *args)
-    helper_code = gen.code()
+        output = io.StringIO()
+        synth.synthesize(source_code, output, print_ast=True, visualize_ast=True)
+        program_code = output.getvalue()
+        output.close()
 
-    helper_path = tmp_path / "helpers.o"
-    program_path = tmp_path / "program.o"
+        gen = FunctionGenerator()
+        for args in functions:
+            func, args = args[0], args[1:]
+            func(gen, *args)
+        helper_code = gen.code()
 
-    Toolchain.compile(helper_path, helper_code)
-    Toolchain.compile(program_path, program_code)
-    Toolchain.link(Path("/dev/null"), helper_path, program_path)
+        helper_path = tmp_path / "helpers.o"
+        program_path = tmp_path / "program.o"
+
+        Toolchain.compile(helper_path, helper_code)
+        Toolchain.compile(program_path, program_code)
+        Toolchain.link(Path("/dev/null"), helper_path, program_path)
